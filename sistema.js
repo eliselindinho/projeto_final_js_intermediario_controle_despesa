75f0cd17babbd46dcb617ece3420bb5c28809c94
elis mendonca <elis_pm@hotmail.com>
2023-03-14 15:47:22 -0300

    Iniciando o js

--- sistema.js
+++ sistema.js
@@ -1,10 +1,13 @@
 let expense = document.querySelector("#expenses");
 let category = document.querySelector("#category");
+let home = document.querySelector("#home");
+let addExpense = document.querySelector("#addExpense");
+let categoryRegister = document.querySelector("#categoryRegister");
+let modalAddEditCategory = document.querySelector("#modalAddEditCategory");
 let monitoringTotalPaid = document.querySelector("#monitoringTotalPaid");
 let monitoringTotalPayable = document.querySelector("#monitoringTotalPayable");
 let monitoringLate = document.querySelector("#monitoringLate");
 let categoryFilter = document.querySelector("#categoryFilterHome");
-let buttonFilter = document.querySelector("#buttonFilter");
 let homePageTable = document.querySelector("#homePageTable");
 let bodyTableHomePage = document.querySelector("#bodyTableHomePage");
 let modalButtonAddExpense = document.querySelector("#modalButtonAddExpense");
@@ -13,10 +16,7 @@ let categoryAddExpense = document.querySelector("#categoryAddExpense");
 let dueDateAddExpense = document.querySelector("#dueDateAddExpense");
 let valueAddExpense = document.querySelector("#valueAddExpense");
 let saveAddExpense = document.querySelector("#saveAddExpense");
-let buttonCancelAddExpense = document.querySelector("#cancelAddExpense");
 let categoryFilterRegister = document.querySelector("#categoryFilterRegister");
-let buttonFilterCategory = document.querySelector("#buttonFilterCategory");
-let addExpense = document.querySelector("#addExpense");
 let headerTableCategoryRegister = document.querySelector(
   "#headerTableCategoryRegister"
 );
@@ -25,5 +25,19 @@ let categoryRegisterTableHeader = document.querySelector(
 );
 let bodyTableAddCategory = document.querySelector("#bodyTableAddCategory");
 let categoryFilterEditAdd = document.querySelector("#categoryFilterEditAdd");
+
+let buttonFilter = document.querySelector("#buttonFilter");
+let buttonCancelAddExpense = document.querySelector("#cancelAddExpense");
+let buttonFilterCategory = document.querySelector("#buttonFilterCategory");
 let buttonSave = document.querySelector("#buttonSave");
 let buttonForCancel = document.querySelector("#buttonForCancel");
+
+function activeExpense(active, desactive) {
+  category.classList.remove("active");
+  expense.classList.add("active");
+}
+
+function activeCategory(active, desactive) {
+  expense.classList.remove("active");
+  category.classList.add("active");
+}
