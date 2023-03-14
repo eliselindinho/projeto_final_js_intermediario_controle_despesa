6f09d71a2e0464e34a18fe7c18907da508178117
elis mendonca <elis_pm@hotmail.com>
2023-03-13 21:11:33 -0300

    Desenvolvendo um controle de despesa online para cliente

---
+++ sistema.js
@@ -0,0 +1,29 @@
+let expense = document.querySelector("#expenses");
+let category = document.querySelector("#category");
+let monitoringTotalPaid = document.querySelector("#monitoringTotalPaid");
+let monitoringTotalPayable = document.querySelector("#monitoringTotalPayable");
+let monitoringLate = document.querySelector("#monitoringLate");
+let categoryFilter = document.querySelector("#categoryFilterHome");
+let buttonFilter = document.querySelector("#buttonFilter");
+let homePageTable = document.querySelector("#homePageTable");
+let bodyTableHomePage = document.querySelector("#bodyTableHomePage");
+let modalButtonAddExpense = document.querySelector("#modalButtonAddExpense");
+let modalAddExpense = document.querySelector("#modalAddExpense");
+let categoryAddExpense = document.querySelector("#categoryAddExpense");
+let dueDateAddExpense = document.querySelector("#dueDateAddExpense");
+let valueAddExpense = document.querySelector("#valueAddExpense");
+let saveAddExpense = document.querySelector("#saveAddExpense");
+let buttonCancelAddExpense = document.querySelector("#cancelAddExpense");
+let categoryFilterRegister = document.querySelector("#categoryFilterRegister");
+let buttonFilterCategory = document.querySelector("#buttonFilterCategory");
+let addExpense = document.querySelector("#addExpense");
+let headerTableCategoryRegister = document.querySelector(
+  "#headerTableCategoryRegister"
+);
+let categoryRegisterTableHeader = document.querySelector(
+  "#categoryRegisterTableHeader"
+);
+let bodyTableAddCategory = document.querySelector("#bodyTableAddCategory");
+let categoryFilterEditAdd = document.querySelector("#categoryFilterEditAdd");
+let buttonSave = document.querySelector("#buttonSave");
+let buttonForCancel = document.querySelector("#buttonForCancel");
