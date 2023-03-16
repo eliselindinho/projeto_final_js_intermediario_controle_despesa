131b38618a9ba0bf1518205f61043ddf1c2e2c98
elis mendonca <elis_pm@hotmail.com>
2023-03-15 21:08:39 -0300

    Criando função para salvar a categoria criada ou editada

--- sistema.js
+++ sistema.js
@@ -1,51 +1,109 @@
-let expense = document.querySelector("#expenses");
-let category = document.querySelector("#category");
-let home = document.querySelector("#home");
-let addExpense = document.querySelector("#addExpense");
-let categoryRegister = document.querySelector("#categoryRegister");
-let modalAddEditCategory = document.querySelector("#modalAddEditCategory");
-let monitoringTotalPaid = document.querySelector("#monitoringTotalPaid");
-let monitoringTotalPayable = document.querySelector("#monitoringTotalPayable");
-let monitoringLate = document.querySelector("#monitoringLate");
-let categoryFilter = document.querySelector("#categoryFilterHome");
-let homePageTable = document.querySelector("#homePageTable");
-let bodyTableHomePage = document.querySelector("#bodyTableHomePage");
-let modalButtonAddExpense = document.querySelector("#modalButtonAddExpense");
-let modalAddExpense = document.querySelector("#modalAddExpense");
-let categoryAddExpense = document.querySelector("#categoryAddExpense");
-let dueDateAddExpense = document.querySelector("#dueDateAddExpense");
-let valueAddExpense = document.querySelector("#valueAddExpense");
-let saveAddExpense = document.querySelector("#saveAddExpense");
-let categoryFilterRegister = document.querySelector("#categoryFilterRegister");
-let headerTableCategoryRegister = document.querySelector(
+const expense = document.querySelector("#expenses");
+const category = document.querySelector("#category");
+const home = document.querySelector("#home");
+const addExpense = document.querySelector("#addExpense");
+const categoryRegister = document.querySelector("#categoryRegister");
+const modalAddEditCategory = document.querySelector("#modalAddEditCategory");
+const monitoringTotalPaid = document.querySelector("#monitoringTotalPaid");
+const monitoringTotalPayable = document.querySelector(
+  "#monitoringTotalPayable"
+);
+const monitoringLate = document.querySelector("#monitoringLate");
+const categoryFilter = document.querySelector("#categoryFilterHome");
+const homePageTable = document.querySelector("#homePageTable");
+const bodyTableHomePage = document.querySelector("#bodyTableHomePage");
+const modalButtonAddExpense = document.querySelector("#modalButtonAddExpense");
+const modalAddExpense = document.querySelector("#modalAddExpense");
+const categoryAddExpense = document.querySelector("#categoryAddExpense");
+const dueDateAddExpense = document.querySelector("#dueDateAddExpense");
+const valueAddExpense = document.querySelector("#valueAddExpense");
+const saveAddExpense = document.querySelector("#saveAddExpense");
+const categoryFilterRegister = document.querySelector(
+  "#categoryFilterRegister"
+);
+const headerTableCategoryRegister = document.querySelector(
   "#headerTableCategoryRegister"
 );
-let categoryRegisterTableHeader = document.querySelector(
+const categoryRegisterTableHeader = document.querySelector(
   "#categoryRegisterTableHeader"
 );
-let bodyTableAddCategory = document.querySelector("#bodyTableAddCategory");
-let categoryFilterEditAdd = document.querySelector("#categoryFilterEditAdd");
+const bodyTableAddCategory = document.querySelector("#bodyTableAddCategory");
+const categoryFilterEditAdd = document.querySelector("#categoryFilterEditAdd");
 
-let buttonFilter = document.querySelector("#buttonFilter");
-let buttonCancelAddExpense = document.querySelector("#cancelAddExpense");
-let buttonFilterCategory = document.querySelector("#buttonFilterCategory");
-let buttonSave = document.querySelector("#buttonSave");
-let buttonForCancel = document.querySelector("#buttonForCancel");
+const buttonFilter = document.querySelector("#buttonFilter");
+const buttonCancelAddExpense = document.querySelector("#cancelAddExpense");
+const buttonFilterCategory = document.querySelector("#buttonFilterCategory");
+const buttonSave = document.querySelector("#buttonSave");
+const buttonForCancel = document.querySelector("#buttonForCancel");
+const buttonAddExpenseTriggerModal = document.querySelector(
+  "#buttonAddExpenseTriggerModal"
+);
+const buttonPageRegisterCategory = document.querySelector(
+  "#buttonPageRegisterCategory"
+);
 
-function activeExpense(active, desactive) {
-  category.classList.remove("active");
-  expense.classList.add("active");
+const numberId = 1000;
+
+// Alternar entre categoria e despesas.
+
+function activeExpense() {
+  categoryRegister.style.display = "none";
+  home.style.display = "block";
 }
 
-expense.addEventListener("click", function activeExpense() {});
+expense.addEventListener("click", activeExpense);
 
-function activeCategory(active, desactive) {
-  expense.classList.remove("active");
-  category.classList.add("active");
+function activeCategory() {
+  categoryRegister.style.display = "block";
+  home.style.display = "none";
 }
 
-category.addEventListener("click", function activeCategory() {});
+category.addEventListener("click", activeCategory);
 
-addExpense.remove();
-categoryRegister.remove();
-modalAddEditCategory.remove();
+// Mostrar e  fechar o modal de adição de despesas.
+
+function showModalAddExpense() {
+  addExpense.style.display = "block";
+}
+
+buttonAddExpenseTriggerModal.addEventListener("click", showModalAddExpense);
+
+function hideModalExpense() {
+  addExpense.style.display = "none";
+}
+
+buttonCancelAddExpense.addEventListener("click", hideModalExpense);
+
+// Ativar o botão de adicionar categoria para mostrar o modal de cadastro e edição.
+
+function editionModalCategory() {
+  modalAddEditCategory.style.display = "block";
+}
+
+buttonPageRegisterCategory.addEventListener("click", editionModalCategory);
+
+// Fechar o modal de cadastro.
+function hideModalCategory() {
+  modalAddEditCategory.style.display = "none";
+}
+
+buttonForCancel.addEventListener("click", hideModalCategory);
+
+// Salvar as categorias que serão criadas.
+
+const arrRegisterCategory = [];
+let numberRegisterId = 1000;
+
+function saveRegisterCategory() {
+  numberRegisterId += 1;
+  const objetCategory = {
+    id: numberRegisterId,
+    nome: categoryFilterEditAdd.value,
+  };
+  arrRegisterCategory.push(objetCategory);
+  console.log(arrRegisterCategory);
+}
+
+buttonSave.addEventListener("click", () => saveRegisterCategory());
+
+// Limpar o input depois que clicar em salvar.
